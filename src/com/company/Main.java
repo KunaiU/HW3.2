bfe437a2bbf67163d146972107bfd6135b328aad
UMAX <kbuonrakyoev@gmail.com>
2021-12-28 17:07:36 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] array = {8, 6, -2, 3, -4, 2};
+
+        for (int i = 0; i < array.length; i++) {   
+            int pos = i;
+            int min = array[i];
+            // цикл выбора наименьшего элемента
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < min) {
+                    pos = j;
+                    min = array[j];
+                }
+            }
+            array[pos] = array[i];
+            array[i] = min;
+
+            System.out.println(Arrays.toString(array));
+
+        }
+    }
+}
+
+
+
